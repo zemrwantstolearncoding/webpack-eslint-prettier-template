3922c890dd1cae43b5865f06796deb19a6c5ebc7
Surojit Bhowmick <133606245+zemrwantstolearncoding@users.noreply.github.com>
2026-01-20 19:06:03 +0000

    Included additional config for possible testing.

--- eslint.config.js
+++ eslint.config.js
@@ -2,3 +2,30 @@ import { defineConfig } from "eslint/config";
 import eslintConfigESLint from "eslint-config-eslint";
 
 export default defineConfig([eslintConfigESLint]);
+
+/*
+
+<--- USE THIS IN CASE YOU WANNA TEST STUFF  AND ALSO INSTALL eslint-plugin-jest (--save-dev of course) --->
+<--- ALSO MAKE SURE TO FOLLOW THE STEPS MENTIONED IN "USING BABEL" IN THE JEST GETTING STARTED DOCS --->
+
+import pluginJest from "eslint-plugin-jest";
+
+export default defineConfig([
+	eslintConfigESLint,
+	{
+		files: ["./src/*.test.js"],
+		plugins: { jest: pluginJest },
+		languageOptions: {
+			globals: pluginJest.environments.globals.globals,
+		},
+		rules: {
+			"jest/no-disabled-tests": "warn",
+			"jest/no-focused-tests": "error",
+			"jest/no-identical-title": "error",
+			"jest/prefer-to-have-length": "warn",
+			"jest/valid-expect": "error",
+		},
+	},
+]);
+
+*/
